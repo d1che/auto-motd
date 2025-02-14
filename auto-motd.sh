5994bd7c4f91b3cdcea0552c935573b111417d32
Willem Deen <hi@willemdeen.com>
2025-02-14 22:09:58 +0100

    [feat] add dependency checker

---
+++ auto-motd.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Make sure script is executed as root
+if [ "$(id -u)" != "0" ]; then
+	echo "This script must be run as root" 1>&2
+	exit 1
+fi
+
+function confirm {
+  while true; do
+    read -p "Do you want to continue (y/n)? " choice
+    case "$choice" in 
+      y|Y ) break;;
+      n|N ) exit 0;;
+      * ) echo "Invalid choice. Please specify \"y\" or \"n\".";;
+    esac
+  done
+}
+
+depList=("figlet")
+depsToInstall=()
+
+for dep in $depList; do
+  if ! command -v $dep 2>&1 >/dev/null; then
+    $depsToInstall+=($dep)
+  fi
+done
+
+if [ ${#depsToInstall[@]} -neq 0 ]; then
+  echo "This program will install the following dependencies: $depsToInstall"
+  confirm
+fi
+
+apt update && {
+  for dep in $depsToInstall; do
+    apt install $dep -y
+  done
+}
